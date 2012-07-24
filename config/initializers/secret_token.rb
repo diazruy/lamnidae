150c9193db99fdef14a926842ca84a198cdafb88
Ruy Diaz <diazruy@gmail.com>
2012-07-24 13:53:27 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Lamnidae::Application.config.secret_token = '0d3e95af8c31ee56dc6b3a0e665f88b71abc1f9bb7741da3b8adbd977d6a5c13d7a320f7a2530a0cad3901a560d2885febbee0c035fd85268bf0ece2538e766c'
