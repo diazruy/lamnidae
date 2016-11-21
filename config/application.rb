f4cfc6166a2864cacaf77658bd9d9759a38887c6
Ruy Diaz <diazruy@gmail.com>
2016-11-20 20:29:21 -0800

    Add AA assets to precompile

--- config/application.rb
+++ config/application.rb
@@ -71,5 +71,7 @@ module Lamnidae
 
 
     config.assets.initialize_on_precompile = false
+    config.assets.precompile += %w[active_admin.css active_admin.js print.css]
+    config.assets.precompile += %w[active_admin/print.css]
   end
 end
